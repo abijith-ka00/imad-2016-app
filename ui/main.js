4fcefe0942f54f642f5ab846a6e7ac40467da3e7
abijith-ka00 <abijithka@gmail.com>
2016-09-21 21:59:31 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,9 +3,15 @@ console.log('Loaded!');
 var element=document.getElementById('main-text');
 element.innerHTML='new value';
 //move the image
-var img=document.getElementById('madi');
+var img=document.getElementById('madi);
+var marginLeft=0;
+function moveRight () {
+    marginLeft = marginLeft + 10;
+    img.style.marginLeft=arginLeft+ 'px';
+};
+
+
 img.onclick = function () {
-    img.style.marginLeft='100px';
-    
+var interval=setInterval(moveRight, 100);
 
 };
