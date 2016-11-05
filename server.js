1bb0b2e1a4f3fa5cb2a566ade6a309310013d86c
abijith-ka00 <abijithka@gmail.com>
2016-11-05 19:15:02 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -108,15 +108,14 @@ app.post('/create-user1', function (req,res){
     var password = req.body.password;
     var salt = crypto.randomBytes(128).toString('hex');
     var dbString = hash(password, salt);
-    pool.query('INSERT INTO "user1"(username, password)VALUES ($1, $2)', [username, dbString], function (err, result){ 
+    pool.query('INSERT INTO "user1"(username, password) VALUES ($1, $2)', [username, dbString], function (err, result) {  
     if (err) {
         res.status(500).send(err.toString());
     } else {
         res.send('User succesfulli created: ' + username);
     
     }
-        
-    });
+});
     
 });
 
