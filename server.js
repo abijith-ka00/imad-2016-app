4dd3a8b83e10e9cf78824e7fc00ded49f9699cef
abijith-ka00 <abijithka@gmail.com>
2016-11-05 18:39:43 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 var crypto = require('crypto');
+var bodyParser = require('body-parser');
 
 var app = express();
 app.use(morgan('combined'));
+app.use(bodyParser.json());
 
 
 var articles= {
@@ -100,6 +102,24 @@ app.get('/hash/:input', function(req, res) {
     var hashedString = hash(req.params.input, 'this-is-some-random-string');
     res.send(hashedString);
 });
+
+app.post('/create-user1', function (req,res){
+    var username = req.body.username;
+    var password = req.body.password;
+    var salt = crypto.getRandomBytes(128).toString('hex');
+    var dbString = hash(password, salt);
+    pool.query('INSERT INTO "user1"(username, password)VALUES ($1, $2)', [username, dbString], function (err, result){ 
+    if (err) {
+        res.status(500).send(err.toString());
+    } else {
+        res.send('User succesfulli created: ' + username);
+    
+    }
+        
+    });
+    
+});
+
 
 app.get('/:articleName', function  (req,  res){
     var articleName= req.params.articleName;
