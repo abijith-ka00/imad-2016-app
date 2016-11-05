c7812195435422c5faddd73b47ea78e4aa2f7500
abijith-ka00 <abijithka@gmail.com>
2016-11-05 20:19:43 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,13 +1,22 @@
 //counter code
-var button = document.getElementById('counter');
-var counter=0;
-
-button.onclick = function () {
+var submit = document.getElementById('submit_btn');
+submit.onclick = function (){
   var request = new XMLHttpRequest();
-  request.onreadystatechange = function () {
-  
-  }
-  counter=  counter + 1;
-  var span= document.getElementById('count');
-  span.innerHTML = counter.toString();
+ request.onreadystatechange = function () {
+ if (request.readyState === XMLHttpRequest.DONE) {
+     if (request.status === 200) {
+         var names = request.responseText;
+         names = JSON.parse(names);
+         var list ='';
+         for (var i=0; i < names.legth; i++) {
+             list == '<li>' + names[i] + '</li>';
+         }
+         var ul = document.getElementById('namelist');
+         ul.innerHTML = list ;
+     }
+ }
+ 
 };
+var nameInput = document.getElementById('name');
+var name = nameInput.value;
+request.open('POST' , 'http://abijith-ka00.imad.hasura-app.io/submit=name?name='+ name, true);
