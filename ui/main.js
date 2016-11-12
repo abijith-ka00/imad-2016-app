f8246d091d4ef96ff2bc3ceaa854d433852d60e6
abijith-ka00 <abijithka@gmail.com>
2016-11-12 14:09:07 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,11 +1,1 @@
-//counter code
-var submit = document.getElementById('submit_btn');
-submit.onclick = function (){
-    
-  var request = new XMLHttpRequest();
- 
-request.open('POST' , 'http://abijith-ka00.imad.hasura-app.io/login', true);
-request.setRequestHeader('Content-Type', 'application/json');
-request.send(JSON.stringify({username : username, password : password}));
-
-};
+console.log('Loaded!');
