dc85b8fbb761860fde6518be02aa8d0d75a429cb
abijith-ka00 <abijithka@gmail.com>
2016-11-22 19:13:53 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,19 +1,20 @@
 
+var express = require('express');
+var morgan = require('morgan'); 
+var path = require('path');
+ var app = express();
+ app.use(morgan('combined'));
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
- 
-app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
-});
 
-app.get('/ui/index.html', function (req, res) {
+app.get('/', function (req, res) {
     res.senFile(path.join(__dirname, 'ui' ,'index.html'));
 });
 
-app.get('/ui/main.js', function (req, res) {
-res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+app.get('/ui/madi.png', function (req, res) {
+res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 })
 
 res.status(500).send(err.toString());
