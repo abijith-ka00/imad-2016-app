a434d6f78f562fa4caabfa881a341259ba2ab855
abijith-ka00 <abijithka@gmail.com>
2016-11-25 19:31:37 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, 'ui' ,'index.html'));
 });
 
-app.get('/News', function (req, res) {
+app.get('/#news', function (req, res) {
     res.send("my contact number is 9497802408")
 });
 
