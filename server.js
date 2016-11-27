2447a3a47d03b86cfe4416f99ef33f62381c0571
abijith-ka00 <abijithka@gmail.com>
2016-11-27 17:11:56 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,11 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, 'ui' ,'index.html'));
 });
+
+app.get('/home', function(req, res) {
+    res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+
+});
 
 app.get('/ui/about.css', function (req, res) {
     res.sendFile(path.join(__dirname, 'ui', 'about.css'));
