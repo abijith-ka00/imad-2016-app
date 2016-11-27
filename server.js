1261a96c0c64e56996f649ee79ee718eabf88952
abijith-ka00 <abijithka@gmail.com>
2016-11-27 14:05:55 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,9 +10,7 @@ app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, 'ui' ,'index.html'));
 });
 
-app.get('/about', function (req, res) {
-    res.sendFile(path.join(__dirname, 'ui', 'about.css'));
-});
+
 
 app.get('/about', function (req, res) {
     res.sendFile(path.join(__dirname, 'ui' , 'about.html'));
